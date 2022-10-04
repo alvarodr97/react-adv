f25d08d571743de6fd9a06e2de80ebaa6ca65e94
Alvaro <alvarodr_97@hotmail.com>
2022-10-04 17:06:33 +0200

    Control Props implementado

--- src/02-component-patterns/hooks/useProduct.tsx
+++ src/02-component-patterns/hooks/useProduct.tsx
@@ -1,12 +1,38 @@
-import { useState } from "react"
+import { useState, useEffect, useRef } from "react";
+import { onChangeArgs, Product } from "../interfaces/interfaces";
 
-export const useProduct = () => {
+interface useProductArgs {
+  product: Product;
+  onChange?: (args: onChangeArgs) => void;
+  value?: number;
+}
 
-  const [counter, setCounter] = useState(0);
+export const useProduct = ({onChange, product, value = 0}: useProductArgs ) => {
+
+  const [counter, setCounter] = useState(value);
+
+  // Ver si el evento esta controlado
+  const isControlled = useRef(!!onChange);
 
   const increaseBy = (value: number) => {
-    setCounter(prev => Math.max(prev + value, 0))
+
+    // Si el evento está controlado
+    if (isControlled.current) {
+      return onChange!({ count: value, product });
+    }
+
+    const newValue = Math.max(counter + value, 0);
+
+    setCounter(newValue);
+
+    onChange && onChange({ count: newValue, product });
+    
   }
+
+  useEffect(() => {
+    setCounter(value);
+  }, [value])
+  
 
   return {
     counter,
